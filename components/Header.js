4ff3897b10a06d812b99a98cb01d7e182e4e2697
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-09-15 15:01:38 +0300

    finishing components

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-import Colors from '../constants/Colors';
-
+import Colors from "../constants/Colors";
+import TitleText from '../components/TitleText'; 
 const Header = (props) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
 };
@@ -15,15 +15,12 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary, 
+    backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
   },
 
-  headerTitle: {
-    color: "black",
-    fontSize: 18,
-  },
+
 });
 
 export default Header;
--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -13,6 +13,8 @@ import Card from "../components/Card";
 import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import Colors from "../constants/Colors";
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const StartGameScreen = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -50,7 +52,10 @@ const StartGameScreen = (props) => {
       <Card style={styles.summaryContainer}>
         <Text>You selected</Text>
         <NumberContainer>{selectedNumber} </NumberContainer>
-        <Button title="START GAME" />
+        <Button
+          title="START GAME"
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
       </Card>
     );
   }
@@ -62,9 +67,9 @@ const StartGameScreen = (props) => {
       }}
     >
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game</Text>
+        <TitleText style={styles.title}>Start a New Game</TitleText>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -119,6 +124,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10,
+    fontFamily: "open-sans-bold",
   },
   button: {
     width: 100,
